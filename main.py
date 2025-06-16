2c4fc03a34bba523e7ff160ec120d7816d2ea53a
CMo1775 <chris.moreno1775@gmail.com>
2025-06-16 12:18:12 -0400

    Update main.py

--- main.py
+++ main.py
@@ -56,4 +56,7 @@ def enrich():
     except Exception as e:
         print("ENRICH ERROR:", str(e))
         return jsonify({"error": str(e)}), 500
+        if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
+
 
