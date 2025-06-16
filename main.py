3278b52039cbb66d30324dffa36b41b9d8ad3e16
CMo1775 <chris.moreno1775@gmail.com>
2025-06-16 14:18:50 -0400

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 @app.route("/enrich", methods=["POST"])
 def enrich():
     try:
-        data = request.json(force=True)
+        data = request..get_json(force=True)
         print("Incoming data:", data)
 
         account_owner = data.get("account_owner", "Unknown")
